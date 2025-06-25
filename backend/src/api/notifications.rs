fc8b9dbe4ba3e5d90b6c40037e5a59bf0feeb541
marcoff181 <filippomarcon66@gmail.com>
2025-06-25 13:45:50 +0200

    read notification

--- backend/src/api/notifications.rs
+++ backend/src/api/notifications.rs
@@ -11,7 +11,7 @@ use crate::{
 };
 
 pub fn get_routes_and_docs(settings: &OpenApiSettings) -> (Vec<rocket::Route>, OpenApi) {
-    openapi_get_routes_spec![settings:get_notifications]
+    openapi_get_routes_spec![settings:get_notifications,read_notification]
 }
 
 #[openapi(tag = "Notifications")]
@@ -26,6 +26,28 @@ fn get_notifications(user: User) -> Result<Json<Vec<Notification>>, Status> {
     match res {
         Ok(v) => Ok(Json(v)),
         Err(Error::NotFound) => Ok(Json(Vec::new())),
+        Err(e) => {
+            error!("error running get notification: {}", e);
+            Err(Status::InternalServerError)
+        }
+    }
+}
+
+#[openapi(tag = "Notifications")]
+#[get("/<nid>/read")]
+fn read_notification(nid: i32, user: User) -> Result<Json<Notification>, Status> {
+    let mut conn = establish_connection();
+
+    let res = diesel::update(
+        notifications::table
+            .filter(notifications::recipient_user_id.eq(user.id))
+            .filter(notifications::id.eq(nid)),
+    )
+    .set(notifications::read.eq(true))
+    .get_result::<Notification>(&mut conn);
+
+    match res {
+        Ok(v) => Ok(Json(v)),
         Err(e) => {
             error!("error running get notification: {}", e);
             Err(Status::InternalServerError)
