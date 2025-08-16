20b3d4e8c4be5f6ffefc5b48d2d9694222864373
marcoff181 <filippomarcon66@gmail.com>
2025-08-16 12:10:49 +0200

    remove stuff from api

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -76,70 +76,22 @@ fn custom_openapi_spec() -> OpenApi {
         info: Info {
             title: "SplitSmart API".to_owned(),
             description: Some("API doc for the SplitSmart app".to_owned()),
-            terms_of_service: Some("----".to_owned()),
-            contact: Some(Contact {
-                name: Some("okapi example".to_owned()),
-                url: Some("https://github.com/GREsau/okapi".to_owned()),
-                email: None,
-                ..Default::default()
-            }),
-            license: Some(License {
-                name: "MIT".to_owned(),
-                url: Some("https://github.com/GREsau/okapi/blob/master/LICENSE".to_owned()),
-                ..Default::default()
-            }),
             version: env!("CARGO_PKG_VERSION").to_owned(),
             ..Default::default()
         },
-        servers: vec![
-            Server {
-                url: "http://127.0.0.1:8000/".to_owned(),
-                description: Some("Localhost".to_owned()),
-                ..Default::default()
-            },
-            Server {
-                url: "https://example.com/".to_owned(),
-                description: Some("Possible Remote".to_owned()),
-                ..Default::default()
-            },
-        ],
+        //servers: vec![
+        //    Server {
+        //        url: "http://127.0.0.1:8000/".to_owned(),
+        //        description: Some("Localhost".to_owned()),
+        //        ..Default::default()
+        //    },
+        //    Server {
+        //        url: "https://example.com/".to_owned(),
+        //        description: Some("Possible Remote".to_owned()),
+        //        ..Default::default()
+        //    },
+        //],
         // Add paths that do not exist in Rocket (or add extra info to existing paths)
-        paths: {
-            map! {
-                "/home".to_owned() => PathItem{
-                get: Some(
-                    Operation {
-                    tags: vec!["HomePage".to_owned()],
-                    summary: Some("This is my homepage".to_owned()),
-                    responses: Responses{
-                        responses: map!{
-                        "200".to_owned() => RefOr::Object(
-                            Response{
-                            description: "Return the page, no error.".to_owned(),
-                            content: map!{
-                                "text/html".to_owned() => MediaType{
-                                schema: Some(SchemaObject{
-                                    instance_type: Some(SingleOrVec::Single(Box::new(
-                                        InstanceType::String
-                                    ))),
-                                    ..Default::default()
-                                }),
-                                ..Default::default()
-                                }
-                            },
-                            ..Default::default()
-                            }
-                        )
-                        },
-                        ..Default::default()
-                    },
-                    ..Default::default()
-                    }
-                ),
-                ..Default::default()
-                }
-            }
-        },
         ..Default::default()
     }
 }
